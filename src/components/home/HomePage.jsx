2757b91410c8d019092c39f8ee8e61620f05c84f
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-11-26 11:47:35 -0700

    Added descriptive text to landing page

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -1,16 +1,36 @@
 import React from "react";
 import mountains from "../../assets/marius-neugebauer-gMigZZOqu7Y-unsplash.jpg";
-import './homePage.css'
+import "./homePage.css";
 
 const HomePage = () => {
   return (
     <div className="container">
       <div className="season-mode">
         <h1>Winter</h1>
-        <hr id="season_mode_seperator"/>
+        <hr id="season_mode_seperator" />
         <h1>Summer</h1>
       </div>
-      <img className="mountain-image" src={mountains} alt="view of mountains" />
+      <div className="image-container">
+        <img
+          className="mountain-image"
+          src={mountains}
+          alt="view of mountains"
+        />
+        <div id="text-container">
+          <h2 style={{ color: "White" }}>Ski Crud</h2>
+          <hr id="title-text-line" />
+        </div>
+        <div id="app-description">
+          <p style={{ color: "white" }}>
+            An application for thrill seekers to track their progress on
+            personal challenges
+          </p>
+        </div>
+      </div>
+      <div className="buttons">
+        <button id="sign-in-button">Sign in</button>
+        <button id="register-button">Register</button>
+      </div>
     </div>
   );
 };
